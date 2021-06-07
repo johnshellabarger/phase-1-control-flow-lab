077553e8f4780b1523fd096f72cea4f9b1d9868a
John <jsjshellz@gmail.com>
2021-06-07 13:57:32 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let results = ''
+
+  if (distance >= 2001 && distance <= 2500){
+    results = "I will gladly take your thirty bucks." 
+  } else if (distance >= 2500){
+    results = "No can do."
+  } else if (distance >= 199){
+    results = "This one is on me!"
+  }
+  return results; 
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city){
+ const results =  (city === 'NYC') ? "Ok, sounds good." : "No go."
+ return results 
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case "generous":
+      return "Thank you so much."
+      break
+    case "not as generous":
+      return "Thank you."
+      break
+    default:
+      return "Bye."
+  }
 }
